8e587cf565a935144f65e0fd0fd53a432c000dfa
Adam Tache <at201@duke.edu>
2016-11-19 18:50:40 -0500

    Added App Engine Flask boilerplate

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-from flask import Flask, request
-import requests
-import json
-import traceback
-import random
+"""`main` is the top level module for your Flask application."""
+
+# Import the Flask Framework
+from flask import Flask
 app = Flask(__name__)
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
 
-token = "EAATFD6LxlrkBAASD6ZAjgZBJ8e3pyi6aEjdV9hxAAygrUwtgMTvixTCRXh5yMQ87XoHZAizv2KtBR7iGvGER8cpKZAZBGSIr1thRFBKpwJDX6wXI5es7ThJQnNnZBNwZBqZBVwASWk8iX5N2xZAFCvwtF7U2pkebxdWYBjhiKFylTBAZDZD"
 
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-  if request.method == 'POST':
-    try:
-      data = json.loads(request.data)
-      text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
-      sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
-      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
-    except Exception as e:
-      print traceback.format_exc() # something went wrong
-  elif request.method == 'GET': # For the initial verification
-    if request.args.get('hub.verify_token') == '<VERIFY_TOKEN_HERE>':
-      return request.args.get('hub.challenge')
-    return "Wrong Verify Token"
-  return "Hello Duke" #Not Really Necessary
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
 
-if __name__ == '__main__':
-  app.run(debug=True)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
