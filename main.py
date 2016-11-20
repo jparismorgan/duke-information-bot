43f6fa926da7804429ce78330e8f21f3e35a4b33
Paris Morgan <parism9@gmail.com>
2016-11-19 19:21:03 -0500

    GET method facebook verification  /webhook

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def hello():
     return 'Hello World!'
 
 
-@app.route('/', methods=['GET'])
+@app.route('/webhook', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
