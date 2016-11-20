ecde3fcc9c24cb76c79e646ed21734ac518f67b2
Adam Tache <at201@duke.edu>
2016-11-19 22:17:42 -0500

    Cleaned up main.py

--- main.py
+++ main.py
@@ -65,37 +65,9 @@ class Main(webapp2.RequestHandler):
         for r in cursor.fetchall():
             self.response.write('{}\n'.format(r))
 
-            @app.route('/', methods=['POST'])
-            def webhook():
-                """Return a friendly HTTP greeting."""
-                data = request.get_json()
-                sender = data['entry'][0]['messaging'][0]['sender']['id']
-                message = data['entry'][0]['messaging'][0]['message']['text']
-                reply(sender, message[::-1])
-
-    # always return 200 to Facebook's original POST request so they know you
-    # handled their request
-
-    process.messenger_post(request)
-    # return "OK", 200
-    #435ab3e9281b9256d2beb3125b71dd01e9a85af6
-
-#old method
-def reply(user_id, msg):
-    """Sends the message to usr_id. """
-    data = {
-        "recipient": {"id": user_id},
-        "message": {"text": msg}
-    }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + FACEBOOK_PAGE_ACCESS_TOKEN, json=data)
-
-    # handle the response to the subrequest you made
-    if not resp.ok:
-        # log some useful info for yourself, for debugging
-        print 'jeepers. %s: %s' % (fb_response.status_code, fb_response.text)
-
-    print(resp.content)
-
+app2 = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
 
 #new method for google cloud
 def send_fb_message(user_id, msg):
